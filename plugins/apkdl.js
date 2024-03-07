c4b1b504daa1b51bfd3c27567a00598e5d3b63e3
abdlko <161826149+abdlko@users.noreply.github.com>
2024-03-07 09:48:02 +0100

    Update apkdl.js

--- plugins/apkdl.js
+++ plugins/apkdl.js
@@ -1,67 +1,116 @@
-import cheerio from 'cheerio';
-import fetch from 'node-fetch';
-import {
-    apkdl,
-    apkcombo,
-    aptoide
-} from '../lib/scraper-apk.js';
+/**
+* @project_name Queen Amdi [WA Multi-device]
+* @author BlackAmda <https://github.com/BlackAmda>
+* @description A WhatsApp based 3ʳᵈ party application that provide many services with a real-time automated conversational experience
+* @link <https://github.com/BlackAmda/QueenAmdi>
+* @version 4.0.8
+* @file  apkdl.js - QueenAmdi APK Downloaders
 
-let handler = async (m, {
-    conn,
-    args,
-    usedPrefix,
-    text,
-    command
-}) => {
+© 2022 Black Amda, ANTECH. All rights reserved.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.*/
 
-    let lister = [
-        "s",
-        "d"
-    ]
+const { AMDI, _default_list_sections, fmmod_com, Language, blackamda_API, apkDL_List } = require('queen_amdi_core/dist/scripts')
+const axios = require("axios")
+const { fmmods_packages } = _default_list_sections
+const Lang = Language.getString('misc');
 
-    let [feature, inputs, inputs_, inputs__, inputs___] = text.split("+")
-    if (!lister.includes(feature)) return m.reply("هذا الامر خاص بتحميل التطبيقات من موقع \napk-dl.com\nيمكنك تحميل التطبيقات من هذا الامر من خلال كتابة\n*.apkdl s+facebook lite*\nبعد ان تحصل على رابط التطبيق تعود للبوت وتكتب له هذا الامر لتحميله\n*.apkdl d+*(رابط التطبيق) \n\n*لوازم*\n" + lister.map((v, index) => "  ○ " + v).join("\n"))
+AMDI({ cmd: "fmmods", desc: "Fouad-whatsapp mods downloader", type: "download", react: "📱" }, (async (amdiWA) => {
+    let { footerTXT, input, prefix, react, reply, sendDocument, sendListMsg } = amdiWA.msgLayout;
 
-    if (lister.includes(feature)) {
+    const fmmods = await fmmod_com();
+    var listInfo = {}
+    listInfo.title = '📱 Fouad-whatsapp mods downloader'
+    listInfo.text = `
+        Download FMWA latest version apk from here.
 
-        if (feature == "s") {
-            if (!inputs) return m.reply("مثال : \n.apkdl s+facebook lite")
-            await m.reply(wait)
-            try {
-                let res = await apkdl.search(inputs)
-                let teks = res.map((item, index) => {
-                    return `🔍 *[ RESULT ${index + 1} ]*
-📰 *Title:* ${item.name}
-🔗 *Url:* ${item.link}`
-                }).filter(v => v).join("\n\n________________________\n\n")
-                await m.reply(teks)
-            } catch (e) {
-                await m.reply('error')
-            }
-        }
+        By original FM Mods site:
+        https://fmmods.com/fouad-whatsapp/
+    `
+    listInfo.buttonTXT = 'Choose a package'
 
-        if (feature == "d") {
-            if (!inputs) return m.reply("مثال\n .apkdl d+https://apk-dl.com/smart-media4u-technology-pteltd/shareit.lite")
-            try {
-                
-                let resl = await apkdl.download(inputs)
-                
-                let cap = "*Name:* " + resl.appname + "\n" + "*Link:* " + resl.link + "\n\n" + wait
-                await conn.sendFile(m.chat, resl.img, "", cap, m)
-                await conn.sendFile(m.chat, resl.link, resl.appname, null, m, true, {
-                    quoted: m,
-                    mimetype: "application/vnd.android.package-archive"
-                })
-            } catch (e) {
-                await m.reply('error')
-            }
-        }
+    if (!input) {
+        return await sendListMsg(listInfo, fmmods_packages(prefix));
     }
-}
-handler.help = ["apkdl"]
-handler.tags = ["applications"]
-handler.command = /^(apkdl)$/i
-handler.premium = false
-export default handler
 
-/* New Line */
+    await react("⬇️");
+    try {
+        switch (input) {
+            case '1':
+                await react("⬆️");
+                await sendDocument({ url: fmmods['com.whatsapp'].link }, { mimetype: 'application/vnd.android.package-archive', fileName: fmmods['com.whatsapp'].name, caption: footerTXT, quoted: true });
+                await react("✔️");
+                break;
+
+            case '2':
+                await react("⬆️");
+                await sendDocument({ url: fmmods['com.fmwhatsapp'].link }, { mimetype: 'application/vnd.android.package-archive', fileName: fmmods['com.fmwhatsapp'].name, caption: footerTXT, quoted: true });
+                await react("✔️");
+                break;
+
+            case '3':
+                await react("⬆️");
+                await sendDocument({ url: fmmods['com.gbwhatsapp'].link }, { mimetype: 'application/vnd.android.package-archive', fileName: fmmods['com.gbwhatsapp'].name, caption: footerTXT, quoted: true });
+                await react("✔️");
+                break;
+
+            case '4':
+                await react("⬆️");
+                await sendDocument({ url: fmmods['com.yowhatsapp'].link }, { mimetype: 'application/vnd.android.package-archive', fileName: fmmods['com.yowhatsapp'].name, caption: footerTXT, quoted: true });
+                await react("✔️");
+                break;
+        }
+    } catch (e) {
+        console.log(e);
+        return await reply("Error".fetchError(e), "❌", 1);
+    }
+}));
+
+
+AMDI({ cmd: "apk", desc: Lang.APKDL_DESC, type: "download", react: "📦" }, (async (amdiWA) => {
+    let { input, isPlaystore, sendCustomButton, sendListMsg, prefix, react, reply } = amdiWA.msgLayout;
+
+    if (!input) return await reply(Lang.APK_EXAMPLE, "❓");
+
+    try {
+        if (input && isPlaystore(input)) {
+            const psAPI = await blackamda_API("playstore", `package=${input}`, amdiWA.botNumberJid);
+            const response = await axios.get(psAPI);
+            const json = response.data
+
+            if (json.status.error) return await reply("Error".fetchError({ message: json.status.message }), "❌", 1);
+            if (json.size.isLarge) return await reply(Lang.OVER_WA_FILE);
+
+            await react("⬇️");
+            const buttons = [
+                { buttonId: `${prefix}ps ${input}`, buttonText: { displayText: 'ℹ️ App Info' }, type: 1 }
+            ]
+
+            const text = `${Lang.APK_TITLE}
+        📚 App name: ${json.app_name}
+        🧰 Version: ${json.version}`
+
+            await react("⬆️");
+            await sendCustomButton(buttons, text, true, "apk", json.dl_link, `${json.app_name}.apk`);
+            return await react("✔️");
+        } else if (input) {
+            const psAPI = await blackamda_API("search", `platform=playstore&name=${input}`, amdiWA.botNumberJid);
+            const response = await axios.get(psAPI);
+            const json = response.data
+
+            if (json.status.error) return await reply("Error".fetchError({ message: json.status.message }), "❌", 1);
+
+            var listInfo = {}
+            listInfo.title = Lang.APK_TITLE
+            listInfo.text = Lang.APK_TXT
+            listInfo.buttonTXT = 'Select app'
+
+            const sections = apkDL_List(prefix, json.data);
+
+            return await sendListMsg(listInfo, sections)
+        }
+    } catch (e) {
+        console.log(e);
+        return await reply("Error".fetchError(e), "❌", 1);
+    }
+}));
